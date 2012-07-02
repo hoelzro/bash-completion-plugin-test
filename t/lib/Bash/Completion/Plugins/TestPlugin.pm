bbefdbb3e8832aca6529719ed9d0d7946843b252
Rob Hoelz <rob@hoelz.ro>
2012-07-02 00:00:48 -0500

    Move no dupes plugin into its own file

--- t/lib/Bash/Completion/Plugins/TestPlugin.pm
+++ t/lib/Bash/Completion/Plugins/TestPlugin.pm
@@ -19,32 +19,4 @@ sub complete {
 }
 
 
-package Bash::Completion::Plugins::TestPluginNoDups;
-
-use strict;
-use warnings;
-use parent 'Bash::Completion::Plugin';
-
-use Bash::Completion::Utils qw(prefix_match);
-
-my @OPTIONS = qw{foo bar baz};
-
-sub should_activate {
-    return [ 'test-plugin-nodup' ];
-}
-
-sub complete {
-    my ( $self, $r ) = @_;
-
-    my %remaining = map { $_ => 1 } @OPTIONS;
-
-    foreach my $arg ($r->args) {
-        next if $arg eq $r->word;
-
-        delete $remaining{$arg};
-    }
-
-    $r->candidates(prefix_match($r->word, keys %remaining));
-}
-
 1;
